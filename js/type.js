377fc52155e86f3dd34031aca0ff07ce43e6ee69
teenjjj <jeanchen0828@gmail.com>
2022-09-17 22:04:20 +0800

    first commit

--- js/type.js
+++ js/type.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded",function(){
     var i = 0;
-    var text = "Pesto Green is a ESG tech company that strives to create a better world.";
+    var text = "Pesto Green is an ESG tech company that strives to create a more sustainable world.";
     var speed = 100;
     function type(){
             document.getElementById("type").innerHTML += text.charAt(i);
